eef0de7bbf37e60a8136992c5bfd412f61ad1af7
Abijith R A <Abijithra2004@gmail.com>
2025-07-24 12:51:05 +0530

    Changed the supabase project account

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // Initialize Supabase client and make it globally available
-const supabaseUrl = 'https://nfnmmehpsomwnexgythn.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mbm1tZWhwc29td25leGd5dGhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4MTU3MjUsImV4cCI6MjA2NzM5MTcyNX0.sp-z2vJjdmdhRRSnc49On5Z0PeCyfX3WLO1dkYnKmUA';
+const supabaseUrl = 'https://czpindulyzkqnonsxtcc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6cGluZHVseXprcW5vbnN4dGNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0MTIwMzUsImV4cCI6MjA2Mjk4ODAzNX0.EZY2o4LGGQ7StjSVrJt6vCgjvI1uD48y26a0JekOJhc';
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
